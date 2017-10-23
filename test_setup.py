84feb17a9b34f3fb93a4a4537834c7a423620567
Quentin McGaw <quentin.mcgaw@gmail.com>
2017-10-23 15:58:15 -0400

    Added unit test (empty) for setup script

---
+++ test_setup.py
@@ -0,0 +1,39 @@
+import unittest
+import setup
+
+class BirthDate(unittest.TestCase):
+    def setUp(self):
+        pass
+        
+    def tearDown(self):
+        pass
+
+    def test_init(self):
+        self.assertEqual(True, True, "")
+        
+    def test_sum(self):
+        self.assertEqual(True, True, "")
+        
+    def test_repr(self):
+        self.assertEqual(True, True, "")     
+class Functions(unittest.TestCase):
+    def setUp(self):
+        pass
+        
+    def tearDown(self):
+        pass
+
+    def test_check_master_password(self):
+        self.assertEqual(True, True, "")
+        
+    def test_check_birthdate(self):
+        self.assertEqual(True, True, "")
+        
+    def test_intestinize(self):
+        self.assertEqual(True, True, "")
+        
+    def test_checksumIsValid(self):
+        self.assertEqual(True, True, "")
+
+    def test_setup(self):
+        self.assertEqual(True, True, "")
