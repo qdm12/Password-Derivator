416763d6bc44d40f8f20f4792e150221b5c587b6
Quentin McGaw <quentin.mcgaw@gmail.com>
2017-10-29 13:56:01 -0400

    Changed passgen to make unit tests work on Python 2 as well

--- test/test_passgen.py
+++ test/test_passgen.py
@@ -20,6 +20,7 @@ class Functions(TestCase):
     def tearDown(self):
         pass
     
+    
     @patch('derivatex.passgen.checksumIsValid')
     @patch('derivatex.passgen.isMasterpassworddigestfilePresent')
     def test_read_masterpassworddigest_success(self, mock_isMasterpassworddigestfilePresent, mock_checksumIsValid):
@@ -86,53 +87,55 @@ class Functions(TestCase):
         masterpassworddigest = b'masterDiGeSt'
         website_name = b'github'
         digest = passgen.intestinize(masterpassworddigest, website_name)
-        self.assertEqual(digest, 'digestDIGESTdigestDIGESTdigest')
+        self.assertEqual(digest, 'digestDIGESTdigestDIGESTdigest')   
         
     def test_find_characterType(self):
-        lowercase = passgen.find_characterType('a')
-        uppercase = passgen.find_characterType('A')
         digit = passgen.find_characterType('0')
+        uppercase = passgen.find_characterType('A')
+        lowercase = passgen.find_characterType('a')        
         symbol = passgen.find_characterType('*')
-        self.assertEqual(lowercase, "lowercase")
-        self.assertEqual(uppercase, "uppercase")
-        self.assertEqual(digit, "digit")
-        self.assertEqual(symbol, "symbol")
+        self.assertEqual(digit, 0)
+        self.assertEqual(uppercase, 1)
+        self.assertEqual(lowercase, 2)        
+        self.assertEqual(symbol, 3)
 
     def test_ensure_digit(self):
-        characterType = "digit"
+        characterType = 0
         password = "Ab*JFDSDFDhg-?KL"
         i = 2
         password = passgen.ensure(characterType, password, i)
         self.assertEqual(password, "Ab0JFDSDFDhg-?KL")
         
-    def test_ensure_lowercase(self):
-        characterType = "lowercase"
-        password = "A0*JFDSDFD68-?KL"
-        i = 2
-        password = passgen.ensure(characterType, password, i)
-        self.assertEqual(password, "A0aJFDSDFD68-?KL")
-        
     def test_ensure_uppercase(self):
-        characterType = "uppercase"
+        characterType = 1
         password = "0b*786fsh9hg-?-8"
         i = 2
         password = passgen.ensure(characterType, password, i)
-        self.assertEqual(password, "0bF786fsh9hg-?-8")
+        self.assertEqual(password, "0bA786fsh9hg-?-8")
+        
+    def test_ensure_lowercase(self):
+        characterType = 2
+        password = "A0*JFDSDFD68-?KL"
+        i = 2
+        password = passgen.ensure(characterType, password, i)
+        self.assertEqual(password, "A0cJFDSDFD68-?KL")
 
     def test_ensure_symbol(self):
-        characterType = "symbol"
+        characterType = 3
         password = "0bA786fsh9hgBCD8"
         i = 2
         password = passgen.ensure(characterType, password, i)
-        self.assertEqual(password, "0b>786fsh9hgBCD8")
-        
+        self.assertEqual(password, "0b#786fsh9hgBCD8")
+    
     """
+    from string import ascii_lowercase
+    from random import choice
     def test_ensure_characters_rangeInput(self):
         # This relies on test_ensure as it is a relatively simple deterministic function        
-        ITER = 1000
+        ITER = 10000
         for i in range(ITER):
             print(i)
-            password = ''.join(choice(ascii_lowercase) for i in range(10))
+            password = ''.join(choice(ascii_lowercase) for i in range(5))
             _ = passgen.ensure_characters(password)
     """
     
@@ -140,31 +143,31 @@ class Functions(TestCase):
         # This relies on test_ensure as it is a relatively simple deterministic function        
         password = "abcd"
         password = passgen.ensure_characters(password)
-        self.assertEqual(password, 'Bb{1')
-        
+        self.assertEqual(password, '!B2d') 
+    
     def test_ensure_characters_lowercase(self):
         # This relies on test_ensure as it is a relatively simple deterministic function        
         password = "password"
         password = passgen.ensure_characters(password)
-        self.assertEqual(password, 'pass0|Bd')
+        self.assertEqual(password, '!0Asword')
              
     def test_ensure_characters_uppercase(self):
         # This relies on test_ensure as it is a relatively simple deterministic function        
         password = "PASSWORD"
         password = passgen.ensure_characters(password)
-        self.assertEqual(password, 'P1!SWaRD')
+        self.assertEqual(password, 'P1SaWO[D')
              
     def test_ensure_characters_digit(self):
         # This relies on test_ensure as it is a relatively simple deterministic function        
         password = "434239043"
         password = passgen.ensure_characters(password)
-        self.assertEqual(password, 'B3|23904b')
+        self.assertEqual(password, 'c34?39A43')
              
     def test_ensure_characters_symbol(self):
         # This relies on test_ensure as it is a relatively simple deterministic function        
-        password = '*$£()*$&*%)*/'
+        password = '*()*&*%)*/'
         password = passgen.ensure_characters(password)
-        self.assertEqual(password, '*$2b)F$&*%)*/')
+        self.assertEqual(password, '*()*B*1)c/')
         
     def test_ensure_characters_fine(self):
         # This relies on test_ensure as it is a relatively simple deterministic function        
@@ -176,7 +179,7 @@ class Functions(TestCase):
         # This relies on test_ensure as it is a relatively simple deterministic function        
         password = "aaaAAA06"
         password = passgen.ensure_characters(password)
-        self.assertEqual(password, 'aaaAA>06')
+        self.assertEqual(password, 'aaaA]A06')
         
     @patch('derivatex.passgen.ensure_characters')
     @patch('derivatex.passgen.intestinize')
@@ -188,5 +191,4 @@ class Functions(TestCase):
         website_name = "test"
         password = passgen.passgen(website_name)
         self.assertEqual(password, "digestDIGESTED1*")
-        
         
